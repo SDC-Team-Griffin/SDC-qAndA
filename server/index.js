309e35372f5c0d3f0176d42130bae0eb83bf52db
Alex Yamaguchi <alexbilly2110@gmail.com>
2023-07-14 18:27:55 -0500

    Serving up static files

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const { PORT } = process.env;
+const path = require('path');
 const express = require('express');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '../../frontendcapstone/dist')));
 
 app.listen(PORT, () => {
   console.log(`Server listening to PORT: ${ PORT }`);
