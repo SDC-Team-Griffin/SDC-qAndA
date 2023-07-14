8f34d6b928b429379309d8a3b0c18d4652119f4d
Alex Yamaguchi <alexbilly2110@gmail.com>
2023-07-14 11:14:29 -0500

    Save

--- db/models.js
+++ db/models.js
@@ -3,13 +3,13 @@ const { USER, DB } = process.env;
 const fs = require('fs');
 const path = require('path');
 
-const csv = require('csv-parser'); // CSV parsing library **
+const csv = require('fast-csv'); // CSV parsing library **
 
 const connectionString = `postgres://${ USER }@localhost:5432/${ DB }`;
 
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize(connectionString, {
-  pool: { acquire: 600000 } // NOTE: increased timeout to 5 min **
+  pool: { acquire: 600000 } // NOTE: increased timeout to 10 min **
 });
 
 /*
@@ -144,108 +144,20 @@ AnswerPhoto.belongsTo(Answer, { foreignKey: 'answer_id' });
 // IMPORT DATA
 const importData = [];
 
-/* NOTE: must process in chunks (too big for Sequelize)
-
-const questionStream = fs.createReadStream(
-  path.join(__dirname, '../csv/questions.csv'),
-  'utf-8'
-);
-const answerStream = fs.createReadStream(
-  path.join(__dirname, '../csv/answers.csv'),
-  'utf-8'
-);
-const productStream = fs.createReadStream(
-  path.join(__dirname, '../csv/product.csv'),
-  'utf-8'
-);
-const photoStream = fs.createReadStream(
-  path.join(__dirname, '../csv/answers_photos.csv'),
-  'utf-8'
-);
-const questionsCSV = fs.readFileSync(
-  path.join(__dirname, '../csv/questions.csv'),
-  'utf-8'
-);
-const answersCSV = fs.readFileSync(
-  path.join(__dirname, '../csv/answers.csv'),
-  'utf-8'
-);
-const productsCSV = fs.readFileSync(
-  path.join(__dirname, '../csv/product.csv'),
-  'utf-8'
-);
-const photosCSV = fs.readFileSync(
-  path.join(__dirname, '../csv/answers_photos.csv'),
-  'utf-8'
-);
-
-// NOTE: exclude header row!
-const questionsData = questionsCSV.split('\n').slice(1);
-const answersData = answersCSV.split('\n').slice(1);
-const productsData = productsCSV.split('\n').slice(1);
-const photosData = photosCSV.split('\n').slice(1);
-
-questionsData.forEach((question) => {
-  const [
-    id, product_id, body, date_written,
-    asker_name, asker_email, reported, helpful
-  ] = question.split(','); // comma-delimited
-
-  importData.push(
-    Question.create({
-      id, product_id, body, date_written,
-      asker_name, asker_email, reported, helpful
-    })
-  );
-});
-
-answersData.forEach((answer) => {
-  const [
-    id, question_id, body, date_written,
-    answerer_name, answerer_email, reported, helpful
-  ] = answer.split(',');
-
-  importData.push(
-    Answer.create({
-      id, question_id, body, date_written,
-      answerer_name, answerer_email, reported, helpful
-    })
-  );
-});
-
-productsData.forEach((product) => {
-  const [ id, name ] = product.split(',');
-
-  importData.push(
-    Product.create({ id, name })
-  );
-});
-
-photosData.forEach((photo) => {
-  const [ id, answer_id, url ] = photo.split(',');
-
-  importData.push(
-    AnswerPhoto.create({ id, answer_id, url })
-  );
-});
-
-return Promise.all(importData);
-*/
-
 const parseCSV = (stream, model, attributes) => {
+
   return new Promise((resolve, reject) => {
     const chunks = [];
 
     let bit = [];
 
     stream
-      .pipe(csv()) // stream —> csv-parser —> JS objs
+      .pipe(csv.parse()) // stream —> fast-csv —> JS objs
       .on('data', (data) => {
-        // chunks.push(data);
         bit.push(data);
 
         // if too big —> offload **
-        if (bit.length >= 100) {
+        if (bit.length >= 50) {
           chunks.push(bit);
           bit = [];
         }
@@ -258,12 +170,12 @@ const parseCSV = (stream, model, attributes) => {
         }
 
         const insertChunks = chunks.map((chunk) => {
+
           const modelData = chunk.map((row) => {
             return Object.fromEntries(
               attributes.map((attr) => [ attr, row[attr] ])
             );
           });
-
           return model.bulkCreate(modelData);
         });
 
@@ -271,32 +183,16 @@ const parseCSV = (stream, model, attributes) => {
           .then(() => resolve())
           .catch((err) => reject(err));
 
-        /*
-        const chunkSize = 20;
-
-        for (let i = 0; i < chunks.length; i += chunkSize) {
-          const chunk = chunks.slice(i, i + chunkSize);
-
-          const modelData = chunk.map((row) => {
-            return Object.fromEntries(
-              attributes.map((attr) => [ attr, row[attr] ])
-            );
-          });
-
-          importData.push(model.bulkCreate(modelData));
-        }
-
-        resolve();
-        */
       })
       .on('error', (err) => reject(err));
   });
 };
 
 (async() => {
+
   try {
-    await sequelize.sync({ force: true });
-    console.log('Tables synced!');
+    await sequelize.sync();
+    console.log('Tables synced successfully!');
 
     const csvFiles = [
       {
@@ -330,10 +226,10 @@ const parseCSV = (stream, model, attributes) => {
 
     // NOTE: only query products w/ questions! **
     const products = await Product.findAll({
-      includes: [{
+      include: {
         model: Question,
         required: true
-      }]
+      }
     });
 
     await sequelize.close();
@@ -343,40 +239,3 @@ const parseCSV = (stream, model, attributes) => {
     console.error(`Error importing data: ${ err }`);
   }
 })();
-
-/* SYNC MODELS
-sequelize
-  .sync({ force: true }) // already exists —> recreate
-  .then(() => {
-    console.log('Tables created successfully!');
-
-    return Promise.all([
-      parseCSV(
-        questionStream,
-        Question,
-        [ 'id', 'product_id', 'body', 'date_written', 'asker_name', 'asker_email', 'reported', 'helpful' ]
-      ),
-      parseCSV(
-        answerStream,
-        Answer,
-        [ 'id', 'question_id', 'body', 'date_written', 'answerer_name', 'answerer_email', 'reported', 'helpful' ]
-      ),
-      parseCSV(
-        productStream,
-        Product,
-        [ 'id', 'name' ]
-      ),
-      parseCSV(
-        photoStream,
-        AnswerPhoto,
-        [ 'id', 'answer_id', 'url' ]
-      )
-    ]);
-  })
-  .then(() => {
-    console.log('Data imported successfully!');
-  })
-  .catch((err) => {
-    console.error(`Error importing data: ${ err }`);
-  });
-*/
