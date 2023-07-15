307b2b5c8cd07e9d2dd65175adc8a84cc6906e88
Alex Yamaguchi <alexbilly2110@gmail.com>
2023-07-15 05:21:23 -0500

    Recreated tables, post req retrieved okay but sequelize unique contraint error

--- server/index.js
+++ server/index.js
@@ -4,17 +4,20 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 
-const { db, QueryTypes } = require('../db/db.js'); // import db (sequelize)
+const { sequelize, QueryTypes } = require('../db/db.js'); // import db (sequelize)
 
 const app = express();
 
+/*
 app.use(express.static(
   path.join(__dirname, '../../frontendcapstone/dist'))
 );
+*/
 
 app.use(morgan('dev'));
 app.use(express.json());
 
+// QUESTIONS
 app.get('/qa/questions', (req, res) => {
   const { product_id, page, count } = req.query;
 
@@ -28,17 +31,48 @@ app.get('/qa/questions', (req, res) => {
     OFFSET ${ offset }
   `;
 
-  db.query(strQuery, { type: QueryTypes.SELECT })
+  sequelize
+    .query(strQuery, { type: QueryTypes.SELECT })
     .then((questions) => {
       console.log('Questions fetched successfully!');
 
-      res.send(questions);
+      res.json(questions);
     })
     .catch((err) => {
-      res.status(500).send(`Error fetching questions: ${ err }`)
+      res.status(500).json({ error: `Error fetching questions: ${ err }` });
     });
 });
 
+app.post('/qa/questions', (req, res) => {
+  const { product_id, body, name, email } = req.body;
+  console.log(`product_id: ${ product_id }, body: ${ body }, name: ${ name }, email: ${ email }`);
+
+  const strQuery = `
+    INSERT INTO questions (product_id, body, asker_name, asker_email)
+    VALUES ($1, $2, $3, $4)
+  `;
+
+  // "SequelizeUniqueConstraintError: Validation error"
+
+  // NOTE: consider validating before INSERT **
+
+  sequelize
+    .query(strQuery, {
+      type: QueryTypes.INSERT,
+      bind: [ product_id, body, name, email ]
+    })
+    .then(() => {
+      res.status(201).json({
+        success: true,
+        message: 'Question posted successfully!'
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: `Error posting question: ${ err }` });
+    });
+});
+
+// ANSWERS
 app.get('/qa/questions/:question_id/answers', (req, res) => {
   const { question_id } = req.params;
   const { page, count } = req.query;
@@ -53,14 +87,15 @@ app.get('/qa/questions/:question_id/answers', (req, res) => {
     OFFSET ${ offset }
   `;
 
-  db.query(strQuery, { type: QueryTypes.SELECT })
+  sequelize
+    .query(strQuery, { type: QueryTypes.SELECT })
     .then((answers) => {
       console.log('Answers fetched successfully!');
 
-      res.send(answers);
+      res.json(answers);
     })
     .catch((err) => {
-      res.status(500).send(`Error fetching answers: ${ err }`);
+      res.status(500).json({ error: `Error fetching answers: ${ err }` });
     });
 });
 
