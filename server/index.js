40393ccdcbecde74b81d3ca4e02f35adc58867ee
Alex Yamaguchi <alexbilly2110@gmail.com>
2023-07-14 21:34:13 -0500

    Fetching answers

--- server/index.js
+++ server/index.js
@@ -39,6 +39,31 @@ app.get('/qa/questions', (req, res) => {
     });
 });
 
+app.get('/qa/questions/:question_id/answers', (req, res) => {
+  const { question_id } = req.params;
+  const { page, count } = req.query;
+
+  const offset = (page - 1) * count;
+
+  const strQuery = `
+    SELECT * FROM answers
+    WHERE question_id = ${ question_id }
+    ORDER BY id
+    LIMIT ${ count }
+    OFFSET ${ offset }
+  `;
+
+  db.query(strQuery, { type: QueryTypes.SELECT })
+    .then((answers) => {
+      console.log('Answers fetched successfully!');
+
+      res.send(answers);
+    })
+    .catch((err) => {
+      res.status(500).send(`Error fetching answers: ${ err }`);
+    });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening to PORT: ${ PORT }`);
 });
