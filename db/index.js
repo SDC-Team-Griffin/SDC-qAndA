a5c59bb71f66d9347677a29ff514f06a8bde8980
Ubuntu <ubuntu@ip-172-31-15-86.us-east-2.compute.internal>
2023-07-22 00:03:35 +0000

    Fixed DB connection issue

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
-const { USER, HOST1, HOST2, DB, PW, DB_PORT } = process.env;
+const { USER, HOST, DB, PW, DB_PORT } = process.env;
 const { Pool } = require('pg');
 
 const pool = new Pool({
   user: USER,
-  host: HOST1,
+  host: HOST,
   database: DB,
   password: PW,
   port: DB_PORT
